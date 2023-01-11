081ab93d6ef2d9480b6aa8b0c5310e2af516d934
Harsha Addanki <harsha7addanki@gmail.com>
2023-01-11 17:29:22 -0500

    gud

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ from PySide6.QtWidgets import (
     QGridLayout,
     QLabel,
     QLineEdit,
+    QFileDialog
 )
+
+# Numpy arrays are more memory efficient that python list so when loading large data sets it won't crash
+import numpy as np
 
 app = QApplication([])
 
-class MainWindow(QMainWindow):
+
+class DatabaseView(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
-        #im kinda stupid so i'm gonna just keep it like that(For Now)
+        # im kinda stupid so i'm gonna just keep it like that(For Now)
         self.setWindowTitle('Database viewer thingy')
         # Soo for a database app we need a verticaL with a grid layout in it
         layout = QVBoxLayout()
@@ -30,22 +35,26 @@ class MainWindow(QMainWindow):
                 gridLayout.addWidget(QLineEdit(f"{i}, {j}"), i, j)
                 j+=1
             i+=1
-        
-        
-        
+
+        layout.addWidget(QLabel("Database Viewer Thingamabober"))
         layout.addWidget(gridWidget)
             
         # And then we need a label in the grid
 
         
-        layout.addWidget(QLabel("Database Viewer Thingamabober"))
+
         layout.addWidget(gridWidget)
         RENDER_WIDGET = QWidget()
         RENDER_WIDGET.setLayout(layout)
         self.setCentralWidget(RENDER_WIDGET)
+class  StartingMenu(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        
 
 
-window = MainWindow()
+
+window = DatabaseView()
 window.show()
 
 app.exec()
