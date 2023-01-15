e6d02b2e1f8f5c37e8bc375fa90ecb3797b99c02
Jojo6500 <akula.tushaar@gmail.com>
2023-01-15 00:40:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import (
-    QApplication, 
+    QApplication,
     QMainWindow,
     QVBoxLayout,
     QWidget,
@@ -12,30 +12,48 @@ from PySide6.QtWidgets import (
 import os
 import numpy as np
 #from .aigenerator import *
-
-
+ 
+ 
+ 
 app = QApplication([])
+nextwin = QMainWindow(thedata=thedata)
 class MainWindow(QMainWindow):
-    def __init__(self, thedata) -> None:
+    def __init__(self,thedata) -> None:
         super().__init__()
+        #im kinda stupid so i'm gonna just keep it like that(For Now)
         self.setWindowTitle("Database viewer")
+        # Soo for a database app we need a verticaL with a grid layout in it
         layout = QVBoxLayout()
+       
+        # And then we need a grid layout in the vertical layout
         layout.addWidget(QLabel("Database Viewer"))
         gridWidget = QWidget()
         gridLayout = QGridLayout()
         gridWidget.setLayout(gridLayout)
-        
-        for i in range(thedata.shape[1]):
-            for j in range(thedata.shape[0]):
-                gridLayout.addWidget(QLineEdit(f"{thedata[j][i]}"), i, j)
-        
+        # And then we need a label in the grid layout for the title
+        i = 0
+        while i < thedata.shape[1]:
+            j = 0
+            while j < thedata.shape[0]:
+                gridLayout.addWidget(QLineEdit(f"{thedata[i-1][j-1]}"), i, j)
+                j+=1
+            i+=1
+       
+       
+       
+        layout.addWidget(gridWidget)
+           
+        # And then we need a label in the grid
+ 
+       
         layout.addWidget(gridWidget)
         RENDER_WIDGET = QWidget()
         RENDER_WIDGET.setLayout(layout)
         self.setCentralWidget(RENDER_WIDGET)
-
+ 
+ 
 class MainMenu(QMainWindow):
-
+ 
     def openFile(self) -> None:
         fileDialog = QFileDialog()
         fileDialog.setNameFilter(("CSV (*.csv)"))
@@ -47,11 +65,12 @@ class MainMenu(QMainWindow):
         fileDialog.setDirectory(os.getcwd())
         if fileDialog.exec() == QFileDialog.Accepted:
             fileName = fileDialog.selectedFiles()[0]
+            print(fileName)
             thedata = np.loadtxt(fileName, delimiter=",", dtype=str)
             self.setVisible(False)
-            nextwin = MainWindow(thedata)
+           
             nextwin.show()
-
+ 
     def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle("Main Menu")
@@ -60,10 +79,17 @@ class MainMenu(QMainWindow):
         openFileButton = QPushButton("Open A .csv file")
         openFileButton.clicked.connect(self.openFile)
         mainLayout.addWidget(openFileButton)
-        
+       
         mainWidget.setLayout(mainLayout)
         self.setCentralWidget(mainWidget)
-
+        # And then we need a label in the grid layout for the title
+ 
 menu = MainMenu()
 menu.show()
+ 
+# window = MainWindow()
+# window.show()
+ 
 app.exec()
+ 
+input('Press ENTER to exit')
