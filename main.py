448c2a593cee57745c3fd9444d9084549f701257
Jojo6500 <akula.tushaar@gmail.com>
2023-01-15 01:48:44 -0500

    rehdgf
    jkhutnj43yk5m4lrw

--- main.py
+++ main.py
@@ -11,23 +11,41 @@ from PySide6.QtWidgets import (
 )
 import os
 import numpy as np
-
+#from .aigenerator import *
+ 
+ 
+ 
 app = QApplication([])
 
 class MainWindow(QMainWindow):
     def __init__(self,thedata) -> None:
         super().__init__()
+        #im kinda stupid so i'm gonna just keep it like that(For Now)
         self.setWindowTitle("Database viewer")
+        # Soo for a database app we need a verticaL with a grid layout in it
         layout = QVBoxLayout()
+       
+        # And then we need a grid layout in the vertical layout
         layout.addWidget(QLabel("Database Viewer"))
         gridWidget = QWidget()
         gridLayout = QGridLayout()
         gridWidget.setLayout(gridLayout)
-        
-        for i in range(thedata.shape[0]):
-            for j in range(thedata.shape[1]):
-                gridLayout.addWidget(QLineEdit(f"{thedata[i][j]}"), i, j)
-        
+        # And then we need a label in the grid layout for the title
+        i = 0
+        while i < thedata.shape[1]:
+            j = 0
+            while j < thedata.shape[0]:
+                gridLayout.addWidget(QLineEdit(f"{thedata[i-1][j-1]}"), i, j)
+                j+=1
+            i+=1
+       
+       
+       
+        layout.addWidget(gridWidget)
+           
+        # And then we need a label in the grid
+ 
+       
         layout.addWidget(gridWidget)
         RENDER_WIDGET = QWidget()
         RENDER_WIDGET.setLayout(layout)
@@ -47,6 +65,7 @@ class MainMenu(QMainWindow):
         fileDialog.setDirectory(os.getcwd())
         if fileDialog.exec() == QFileDialog.Accepted:
             fileName = fileDialog.selectedFiles()[0]
+            print(fileName)
             thedata = np.loadtxt(fileName, delimiter=",", dtype=str)
             self.setVisible(False)
             nextwin = MainWindow(thedata=thedata)
@@ -63,7 +82,12 @@ class MainMenu(QMainWindow):
        
         mainWidget.setLayout(mainLayout)
         self.setCentralWidget(mainWidget)
+        # And then we need a label in the grid layout for the title
  
 menu = MainMenu()
 menu.show()
+
+ 
 app.exec()
+ 
+
